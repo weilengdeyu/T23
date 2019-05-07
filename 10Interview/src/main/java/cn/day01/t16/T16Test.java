a635e41960d96a5dae4103285dcb9eaa5e445579
Happy <yymqqc@126.com>
2019-05-07 10:10:19 +0800

    修正第二版，解决日期显示格式问题

---
+++ 10Interview/src/main/java/cn/day01/t16/T16Test.java
@@ -0,0 +1,10 @@
+package cn.day01.t16;
+
+/**
+ * @Classname T16Test
+ * @Description 微冷的雨训练营 www.cnblogs.com/weilengdeyu
+ * @Date 2019/5/7 10:09
+ * @Created by Happy-微冷的雨
+ */
+public class T16Test {
+}
